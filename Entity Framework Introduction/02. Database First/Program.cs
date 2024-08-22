2b12670cd58420ad0a73ea7aac8e3c3a49c69738
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-08-22 08:18:11 +0300

    Add files via upload

---
+++ Entity Framework Introduction/02. Database First/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SoftUni{
+
+public class Program{
+    public static void Main(string[] args){
+
+        // SoftUniContext context = new SoftUniContext();
+   
+        // var emp1 = context.Employees.Find(1);
+
+        // Console.Writeline(emp1.FirstName);
+
+
+    }
+}
+}
