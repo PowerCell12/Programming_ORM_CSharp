b4ef65854423863fff9734920030f5316fee1dc4
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-08-24 12:18:00 +0300

    Add files via upload

---
+++ Entity Framework Introduction/03. Employees Full Information/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni{
+
+
+public class Program{
+    public static void Main(string[] args){
+        SoftUniContext context = new SoftUniContext();
+
+
+        Console.WriteLine(StartUp.GetEmployeesFullInformation(context)) ;
+    }
+}
+}
+
+
+
+
+public class StartUp{
+
+
+    public static string GetEmployeesFullInformation(SoftUniContext context){
+
+        var array = new List<string>();
+        var employees = context.Employees.OrderBy(x => x.EmployeeId);
+
+        foreach (var employee in employees){
+
+            array.Add($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+
+        }
+
+        return string.Join("\n", array);
+
+
+    }
+
+}
