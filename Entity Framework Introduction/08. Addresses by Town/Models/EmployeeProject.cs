8fb67d6a3e853df89a34e0ae30af0d477f3685f5
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-08-31 13:07:31 +0300

    Add files via upload

---
+++ Entity Framework Introduction/08. Addresses by Town/Models/EmployeeProject.cs
@@ -0,0 +1,22 @@
+
+namespace SoftUni.Models
+{
+
+    public class EmployeeProject{
+
+
+        public int EmployeeId { get; set;}
+
+        public virtual Employee Employee{ get; set;}
+
+
+        public int ProjectId {get; set;}
+
+        public Project Project {get; set;}
+
+
+
+
+    }
+
+}
