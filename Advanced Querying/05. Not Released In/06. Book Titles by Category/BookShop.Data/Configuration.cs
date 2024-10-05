33530e6f84b60880e2272d35b9bb0e6ecb46ac42
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-05 09:32:25 +0300

    Delete Advanced Querying/05. Not Released In/06. Book Titles by Category directory

--- Advanced Querying/05. Not Released In/06. Book Titles by Category/BookShop.Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookShop.Data
-{
-    internal class Configuration
-    {
-        internal static string ConnectionString
-            => "Server=.;Database=BookShop;TrustServerCertificate=True;User Id=sa;Password=eMk54#k49K<";
-    }
-}
