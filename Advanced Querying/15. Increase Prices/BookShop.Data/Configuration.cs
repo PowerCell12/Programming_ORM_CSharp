11685cf1762e1bfa8d448dd6e3316385d5396c32
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-09 10:40:45 +0300

    Add files via upload

---
+++ Advanced Querying/15. Increase Prices/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=.;Database=BookShop;TrustServerCertificate=True;User Id=sa;Password=eMk54#k49K<";
+    }
+}
