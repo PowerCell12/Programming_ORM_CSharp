d6134d06ed776189aa98c147578f55553264b670
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-11 11:00:47 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/01. Import Users/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new();
+            string data = File.ReadAllText("Datasets/users.json");
+            Console.WriteLine(ImportUsers(context, data));
+        }
+    
+    
+        public static string ImportUsers(ProductShopContext context, string inputJson){
+            List<User> json = JsonConvert.DeserializeObject<List<User>>(inputJson);
+            context.Users.AddRange(json);
+
+            context.SaveChanges();
+            return $"Successfully imported {json.Count()}";
+        }
+
+
+
+
+    }
+}
