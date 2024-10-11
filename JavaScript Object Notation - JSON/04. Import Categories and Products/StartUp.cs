a884d5e4c0c72a7b8b58334bdcb98e23d8afae9a
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-11 11:02:54 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/04. Import Categories and Products/StartUp.cs
@@ -0,0 +1,74 @@
+﻿using System.Net.WebSockets;
+using Newtonsoft.Json;
+using ProductShop.Data;
+// using ProductShop.Dtos;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+            string data = File.ReadAllText("../../../Datasets/categories.json");
+            Console.WriteLine(ImportProducts(context, data));
+        }
+    
+    
+        public static string ImportUsers(ProductShopContext context, string inputJson){
+            List<User> json = JsonConvert.DeserializeObject<List<User>>(inputJson);
+            context.Users.AddRange(json);
+
+            context.SaveChanges();
+            return $"Successfully imported {json.Count()}";
+        }
+
+
+        public static string ImportProducts(ProductShopContext context, string inputJson){
+            var products = JsonConvert.DeserializeObject<Product[]>(inputJson);
+
+            if (products != null){
+
+                context.Products.AddRange(products);
+                context.SaveChanges();
+            }
+
+            return $"Successfully imported {products?.Length}";
+        
+        }
+
+
+        public static string ImportCategories(ProductShopContext context, string inputJson){
+
+            var json = JsonConvert.DeserializeObject<List<Category>>(inputJson).Where(x => x.Name != null).ToList();
+
+
+            if (json != null){
+                context.Categories.AddRange(json);
+                context.SaveChanges();
+            }
+
+            return  $"Successfully imported {json.Count}";
+
+        }
+
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson){
+
+            var cat_pr = JsonConvert.DeserializeObject<List<CategoryProduct>>(inputJson);
+
+            if (cat_pr != null){
+                context.CategoriesProducts.AddRange(cat_pr);
+                context.SaveChanges();
+            }
+
+
+            return $"Successfully imported {cat_pr?.Count}";;
+
+        }
+
+
+
+    }
+}
