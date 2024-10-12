50d5eccd04514cfc3295f0fadd099af7a31ee000
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-12 13:04:24 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/05. Export Products In Range/Data/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            @"Server=.;Database=BookShop;TrustServerCertificate=True;User Id=sa;Password=eMk54#k49K<";
+
+    }
+}
---
+++ JavaScript Object Notation - JSON/05. Export Products In Range/StartUp.cs
@@ -0,0 +1,94 @@
+﻿using System.Diagnostics.Contracts;
+using System.Text.Json;
+using Microsoft.Data.SqlClient.Server;
+using Microsoft.EntityFrameworkCore;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            ProductShopContext context = new();
+
+            string inputJson = File.ReadAllText("Datasets/users.json");
+
+            Console.WriteLine(GetProductsInRange(context));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson){
+
+            var jsonObjects = JsonSerializer.Deserialize<List<User>>(inputJson);
+
+            context.Users.AddRange(jsonObjects);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {jsonObjects.Count}";
+
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson){
+
+            List<Product> products = JsonSerializer.Deserialize<List<Product>>(inputJson);
+
+            if (products != null){
+                context.Products.AddRange(products);
+
+                context.SaveChanges();
+            }
+
+            return $"Successfully imported {products?.Count}";
+
+        }
+
+
+
+        public static string ImportCategories(ProductShopContext context, string inputJson){
+
+            var categories = JsonSerializer.Deserialize<List<Category>>(inputJson).Where(x => x.name != null).ToList();
+
+            context.AddRange(categories);
+
+            context.SaveChanges();
+            return $"Successfully imported {categories.Count}";
+
+
+        }
+
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson){
+
+            var CategoriesProducts = JsonSerializer.Deserialize<List<CategoryProduct>>(inputJson);
+
+            context.AddRange(CategoriesProducts);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {CategoriesProducts.Count}";
+        }
+
+
+        public static string GetProductsInRange(ProductShopContext context){
+
+            var products  = context.Products.Where(x => x.Price >= 500 && x.Price <= 1000)
+            .Select(x => new {
+                name = x.Name,
+                price = x.Price,
+                seller = $"{x.Seller.FirstName} {x.Seller.LastName}"
+            })
+            .OrderBy(x => x.price);
+
+          
+
+            return JsonSerializer.Serialize(products, new JsonSerializerOptions {
+                WriteIndented = true
+            });
+
+        }
+
+    }
+}
