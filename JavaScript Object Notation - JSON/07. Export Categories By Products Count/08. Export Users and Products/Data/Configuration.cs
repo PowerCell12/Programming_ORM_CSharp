0cd6d39dc17874d1a8aed5e411152e9b66d38b10
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-13 11:59:55 +0300

    Delete JavaScript Object Notation - JSON/07. Export Categories By Products Count/08. Export Users and Products directory

--- JavaScript Object Notation - JSON/07. Export Categories By Products Count/08. Export Users and Products/Data/Configuration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductShop.Data
-{
-    public static class Configuration
-    {
-        public const string ConnectionString =
-            @"Server=.;Database=BookShop;TrustServerCertificate=True;User Id=sa;Password=eMk54#k49K<";
-
-    }
-}
