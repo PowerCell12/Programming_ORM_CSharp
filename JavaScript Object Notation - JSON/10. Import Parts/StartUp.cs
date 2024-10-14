0358203ae759e9b8716340ac8cb1015763abd396
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-14 10:41:06 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/10. Import Parts/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using CarDealer.Data;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new();
+            string inputJson = File.ReadAllText("Datasets/suppliers.json");
+
+            Console.WriteLine(ImportSuppliers(context, inputJson));
+        }
+
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson){
+
+
+            var files = JsonSerializer.Deserialize<List<Supplier>>(inputJson);
+
+            context.Suppliers.AddRange(files);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {files.Count}.";;
+        }
+
+
+        public static string ImportParts(CarDealerContext context, string inputJson){
+
+            var parts = JsonSerializer.Deserialize<List<Part>>(inputJson);
+
+            context.Parts.AddRange(parts);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count}.";;
+        }
+
+
+    }
+}
