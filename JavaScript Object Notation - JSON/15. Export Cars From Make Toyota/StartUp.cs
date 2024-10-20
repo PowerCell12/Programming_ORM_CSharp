77c5dcbe6dd1f437777814dbc1fd643ac55d7de1
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-20 13:10:41 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/15. Export Cars From Make Toyota/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using CarDealer.Data;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            CarDealerContext context = new();
+
+            Console.WriteLine(GetOrderedCustomers(context));
+
+        }
+
+
+         public static string GetOrderedCustomers(CarDealerContext context){
+
+            
+            var customers = context.Customers
+            .OrderBy(x => x.BirthDate)
+            .ThenBy(x => x.IsYoungDriver == true)
+            .Select(x => new {
+                Name = x.Name,
+                BirthDate = x.BirthDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture),
+                IsYoungDriver = x.IsYoungDriver
+            });
+
+
+
+            return JsonSerializer.Serialize(customers, new JsonSerializerOptions{
+                WriteIndented = true
+            }); 
+        }
+
+        public static string GetCarsFromMakeToyota(CarDealerContext context){
+
+            var cars = context.Cars.Where(x => x.Make == "Toyota")
+            .Select(x => new {
+                x.Id,
+                x.Make,
+                x.Model,
+                x.TraveledDistance
+            })
+            .OrderBy(x => x.Model)
+            .ThenByDescending(x => x.TraveledDistance);
+
+            return JsonSerializer.Serialize(cars, new JsonSerializerOptions{
+                WriteIndented = true
+            });
+        }
+    
+    
+    }
+}
