7f7e464fd77256c1dafff6575ad97680fd43fb3d
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-20 13:11:53 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/18. Export Total Sales By Customer/StartUp.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using CarDealer.Data;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            CarDealerContext context = new();
+
+            Console.WriteLine(GetOrderedCustomers(context));
+
+        }
+
+
+         public static string GetOrderedCustomers(CarDealerContext context){
+
+            
+            var customers = context.Customers
+            .OrderBy(x => x.BirthDate)
+            .ThenBy(x => x.IsYoungDriver == true)
+            .Select(x => new {
+                Name = x.Name,
+                BirthDate = x.BirthDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture),
+                IsYoungDriver = x.IsYoungDriver
+            });
+
+
+
+            return JsonSerializer.Serialize(customers, new JsonSerializerOptions{
+                WriteIndented = true
+            }); 
+        }
+
+        public static string GetCarsFromMakeToyota(CarDealerContext context){
+
+            var cars = context.Cars.Where(x => x.Make == "Toyota")
+            .Select(x => new {
+                x.Id,
+                x.Make,
+                x.Model,
+                x.TraveledDistance
+            })
+            .OrderBy(x => x.Model)
+            .ThenByDescending(x => x.TraveledDistance);
+
+            return JsonSerializer.Serialize(cars, new JsonSerializerOptions{
+                WriteIndented = true
+            });
+        }
+    
+    
+        public static string GetTotalSalesByCustomer(CarDealerContext context){
+
+            var customers = context.Customers.Where(x => x.Sales.Count() >= 1)
+            .Select(x => new {
+                fullName = x.Name,
+                boughtCars = x.Sales.Count(),
+                spentMoney = x.Sales.Sum(y => y.Car.PartsCars.Sum(z => z.Part.Price))
+            })
+            .OrderByDescending(x => x.spentMoney)
+            .ThenByDescending(x => x.boughtCars);
+
+
+            return JsonSerializer.Serialize(customers, new JsonSerializerOptions{
+                WriteIndented = true
+            });
+
+        }
+    }
+}
