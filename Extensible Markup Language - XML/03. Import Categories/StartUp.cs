4a7eb267cefe4aa5e69713ee2ba36666cbb126b0
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-22 22:52:12 +0300

    Add files via upload

---
+++ Extensible Markup Language - XML/03. Import Categories/StartUp.cs
@@ -0,0 +1,82 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Xml.Serialization;
+using AutoMapper;
+using AutoMapper.Execution;
+using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+
+        public static Mapper Mapper(){
+            var mapper = new MapperConfiguration(cfg => {
+                cfg.CreateMap<UserDto, User>();
+                cfg.CreateMap<ProductDto, Product>();
+                cfg.CreateMap<CategoryDto, Category>();
+            });
+
+            return new Mapper(mapper);
+        }
+
+
+        public static string ImportUsers(ProductShopContext context, string inputXml){
+
+
+            var serliazer = new XmlSerializer(typeof(UserDto[]), new XmlRootAttribute("Users"));
+
+            var reader = new StringReader(inputXml);
+
+            UserDto[] users = (UserDto[])serliazer.Deserialize(reader);
+
+            var mapped = Mapper().Map<User[]>(users);
+
+            context.Users.AddRange(mapped);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {mapped.Length}";;
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputXml){
+
+            var serliazer = new XmlSerializer(typeof(ProductDto[]), new XmlRootAttribute("Products"));
+
+            var reader = new StringReader(inputXml);
+
+            ProductDto[] products = (ProductDto[])serliazer.Deserialize(reader);
+
+            var mapped = Mapper().Map<Product[]>(products);
+
+            context.Products.AddRange(mapped);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {mapped.Length}";;
+        }
+
+
+        public static string ImportCategories(ProductShopContext context, string inputXml){
+
+            var serliazer = new XmlSerializer(typeof(CategoryDto[]), new XmlRootAttribute("Categories"));
+
+            var reader = new StringReader(inputXml);
+
+            CategoryDto[] categories = (CategoryDto[])serliazer.Deserialize(reader);
+
+            var mapped = Mapper().Map<Category[]>(categories.Where(x => x.Name != null));
+
+            context.Categories.AddRange(mapped);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {mapped.Length}";;
+        }
+    }
+}
