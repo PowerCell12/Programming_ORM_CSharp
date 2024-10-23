87c1cb46aa3035b6d3b427d90c709fe017579f30
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-23 10:46:04 +0300

    Add files via upload

---
+++ Extensible Markup Language - XML/09. Import Suppliers/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace CarDealer.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=.;Database=CarDealer;TrustServerCertificate=True;User Id=sa;Password=eMk54#k49K<";
+    }
+}
---
+++ Extensible Markup Language - XML/09. Import Suppliers/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using CarDealer.Data;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new CarDealerContext();
+            string xml = File.ReadAllText("Datasets/suppliers.xml");
+
+            Console.WriteLine(ImportSuppliers(context, xml));
+
+        }
+
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml){
+
+            XDocument document = XDocument.Parse(inputXml);
+            int count = 0;
+
+            foreach(var element1 in document.Elements()){
+
+                foreach( var element in element1.Elements()){
+                    var el1 = element.Element("name").Value;
+                    var el2 = element.Element("isImporter").Value;
+
+                    Supplier supplier = new()
+                    {
+                        Name = el1,
+                        IsImporter = el2 == "true"
+                    };
+
+                    context.Suppliers.Add(supplier);
+                    context.SaveChanges();
+
+
+                    count++;
+                }
+            }
+
+            return $"Successfully imported {count}";;
+        }
+
+    }
+}
